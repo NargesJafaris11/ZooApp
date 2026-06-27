59e2b9a4b766123e9c42e0a958f3e9c6af1cdbfb
nargesjafari <nargesjafarie@gmail.com>
2026-06-27 22:50:19 +0200

    Added validation to Animal forms

--- ZooApp/Models/Animal.cs
+++ ZooApp/Models/Animal.cs
@@ -1,19 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ZooApp.Models;
 
 public class Animal
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Naam is verplicht.")]
+    [StringLength(50, ErrorMessage = "Naam mag maximaal 50 tekens zijn.")]
     public string Name { get; set; } = "";
 
+    [Required(ErrorMessage = "Diersoort is verplicht.")]
+    [StringLength(50, ErrorMessage = "Diersoort mag maximaal 50 tekens zijn.")]
     public string Species { get; set; } = "";
 
+    [Range(0, 100, ErrorMessage = "Leeftijd moet tussen 0 en 100 zijn.")]
     public int Age { get; set; }
-    
+
     // Foreign Key
     public int? CategoryId { get; set; }
-    
-    
+
     // Navigation Property
     public Category? Category { get; set; }
 }
