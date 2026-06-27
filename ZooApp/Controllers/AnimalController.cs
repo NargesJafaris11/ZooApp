0c2ab359c5f86b4d1b9391e77483c3be26bcd3cc
nargesjafari <nargesjafarie@gmail.com>
2026-06-27 12:42:04 +0200

    Implement animal create functionality

--- ZooApp/Controllers/AnimalController.cs
+++ ZooApp/Controllers/AnimalController.cs
@@ -5,27 +5,20 @@ namespace ZooApp.Controllers;
 
 public class AnimalController : Controller
 {
+    private static List<Animal> animals = new()
+    {
+        new Animal { Id = 1, Name = "Simba", Species = "Lion", Age = 5 },
+        new Animal { Id = 2, Name = "Dumbo", Species = "Elephant", Age = 12 },
+        new Animal { Id = 3, Name = "Melman", Species = "Giraffe", Age = 7 }
+    };
+
     public IActionResult Index()
     {
-        List<Animal> animals = new()
-        {
-            new Animal { Id = 1, Name = "Simba", Species = "Lion", Age = 5 },
-            new Animal { Id = 2, Name = "Dumbo", Species = "Elephant", Age = 12 },
-            new Animal { Id = 3, Name = "Melman", Species = "Giraffe", Age = 7 }
-        };
-
         return View(animals);
     }
-    
+
     public IActionResult Details(int id)
     {
-        List<Animal> animals = new()
-        {
-            new Animal { Id = 1, Name = "Simba", Species = "Lion", Age = 5 },
-            new Animal { Id = 2, Name = "Dumbo", Species = "Elephant", Age = 12 },
-            new Animal { Id = 3, Name = "Melman", Species = "Giraffe", Age = 7 }
-        };
-
         Animal? animal = animals.FirstOrDefault(a => a.Id == id);
 
         if (animal == null)
@@ -35,15 +28,18 @@ public class AnimalController : Controller
 
         return View(animal);
     }
-    
+
     public IActionResult Create()
     {
         return View();
     }
-    
+
     [HttpPost]
     public IActionResult Create(Animal animal)
     {
+        animal.Id = animals.Max(a => a.Id) + 1;
+        animals.Add(animal);
+
         return RedirectToAction(nameof(Index));
     }
 }
