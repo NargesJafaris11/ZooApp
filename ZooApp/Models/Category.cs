f93d1444736751bc58fe235e9d0f64d400427990
nargesjafari <nargesjafarie@gmail.com>
2026-06-28 12:50:22 +0200

    Added validation to Category model

--- ZooApp/Models/Category.cs
+++ ZooApp/Models/Category.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ZooApp.Models;
 
 public class Category
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Categorienaam is verplicht.")]
+    [StringLength(50, ErrorMessage = "Categorienaam mag maximaal 50 tekens zijn.")]
     public string Name { get; set; } = "";
 
     public List<Animal> Animals { get; set; } = new();
